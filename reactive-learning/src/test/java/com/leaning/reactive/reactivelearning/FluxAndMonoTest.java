468db81bbcf45e57aad2c018582290b02b8c9a95
pradeepadi88 <pradeepadi88@gmail.com>
2019-07-04 11:55:16 -0500

    reactive learning - initial commit

--- reactive-learning/src/test/java/com/leaning/reactive/reactivelearning/FluxAndMonoTest.java
+++ reactive-learning/src/test/java/com/leaning/reactive/reactivelearning/FluxAndMonoTest.java
@@ -1,4 +1,16 @@
 package com.leaning.reactive.reactivelearning;
 
+import org.junit.Test;
+import reactor.core.publisher.Flux;
+
 public class FluxAndMonoTest {
+
+    @Test
+    public void fluxTest() {
+        Flux<String> stringFlux = Flux.just("I", "LOVE", "SPRING-BOOT")
+                                    .concatWith(Flux.error(new RuntimeException("Error Occured")));
+        stringFlux
+                .subscribe(System.out::println, (e) -> e.printStackTrace());
+    }
 }
+[d2'sq']
---
+++ reactive-learning/src/test/java/com/leaning/reactive/reactivelearning/ReactiveLearningApplicationTests.java
@@ -0,0 +1,16 @@
+package com.leaning.reactive.reactivelearning;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ReactiveLearningApplicationTests {
+
+	@Test
+	public void test() {
+	}
+
+}
