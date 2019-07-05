d15c253e8e0920a3010324e88bab168d59bc89d5
pradeepadi88 <pradeepadi88@gmail.com>
2019-07-05 04:11:27 -0500

    Test cases for the Reactor Samples

--- reactive-learning/src/test/java/com/leaning/reactive/reactivelearning/FluxAndMonoTest.java
+++ reactive-learning/src/test/java/com/leaning/reactive/reactivelearning/FluxAndMonoTest.java
@@ -2,15 +2,53 @@ package com.leaning.reactive.reactivelearning;
 
 import org.junit.Test;
 import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
 
 public class FluxAndMonoTest {
 
     @Test
     public void fluxTest() {
         Flux<String> stringFlux = Flux.just("I", "LOVE", "SPRING-BOOT")
-                                    .concatWith(Flux.error(new RuntimeException("Error Occured")));
+                                   /* .concatWith(Flux.error(new RuntimeException("Error Occured")))*/
+                                    .log();
         stringFlux
-                .subscribe(System.out::println, (e) -> e.printStackTrace());
+                .subscribe(System.out::println, (e) -> e.printStackTrace(), () -> System.out.println("On Complete"));
     }
+
+    @Test
+    public void fluxTestElements_withoutError() {
+        Flux<String> stringFlux = Flux.just("I", "LOVE", "SPRING-REACTOR")
+                                    .log();
+        StepVerifier
+                .create(stringFlux)
+                .expectNext("I", "LOVE", "SPRING-REACTOR")
+                .verifyComplete();
+    }
+
+    @Test
+    public void fluxTestElements_withError() {
+        Flux<String> stringFlux = Flux.just("I", "LOVE", "SPRING-REACTOR")
+                                        .concatWith(Flux.error(new RuntimeException("Error in Processing")))
+                                        .log();
+        StepVerifier
+                .create(stringFlux)
+                .expectNext("I", "LOVE", "SPRING-REACTOR")
+                .expectError(RuntimeException.class)
+                .verify();
+
+    }
+
+    @Test
+    public void fluxTestElements_withErrorMessages() {
+        Flux<String> stringFlux = Flux.just("I", "LOVE", "SPRING-REACTOR")
+                                        .concatWith(Flux.error(new RuntimeException("Test Error Message")))
+                                        .log();
+        StepVerifier
+                .create(stringFlux)
+                .expectNext("I", "LOVE", "SPRING-REACTOR")
+                .expectErrorMessage("Test Error Message")
+                .verify();
+    }
+
+
 }
-[d2'sq']
